a81059886baff80efa358bc0ccefec837f0d49d5
mizuno <mifukata-nozumi@outlook.jp>
2022-11-16 20:35:49 +0900

    algorithmに関するテスト削除

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <utility.hpp>
 #include <type.hpp>
 #include <table.hpp>
 
@@ -20,9 +19,6 @@ using samples_def = tudb::define_table<
     >
 >;
 
-template <class T1, class T2> struct get_max_size_type : std::conditional<(sizeof(T1) > sizeof(T2)), T1, T2> {};
-template <class T1, class T2> struct get_min_size_type : std::conditional<(sizeof(T1) <= sizeof(T2)), T1, T2> {};
-
 int main()
 {
     constexpr auto concated_str = tudb::concat(tudb::cstr{"abudffjd"}, tudb::cstr{"abcd"}, tudb::cstr{"zyx"});
@@ -36,21 +32,8 @@ int main()
         << create_at_info.name.buf << ' '
         << concated_str.buf << converted_str.buf << std::endl;
 
-    using find_success = tudb::find_if_by_type<int, std::is_same, void, std::string, int, double>::type;
-    using find_failur = tudb::find_if_by_type<int, std::is_same, std::string, double>::type;
-
-    using max_l = tudb::foldl_by_type_t<get_max_size_type, long, long long, short, unsigned short, unsigned long long, unsigned long>;
-    using max_r = tudb::foldr_by_type_t<get_max_size_type, long, long long, short, unsigned short, unsigned long long, unsigned long>;
-    using min_l = tudb::foldl_by_type_t<get_min_size_type, long, long long, short, unsigned short, unsigned long long, unsigned long>;
-    using min_r = tudb::foldr_by_type_t<get_min_size_type, long, long long, short, unsigned short, unsigned long long, unsigned long>;
-
-    constexpr auto aa = tudb::is_unique<int, double, std::string>::value;
-    constexpr auto bb = tudb::is_unique<int, double, std::true_type, std::string, double>::value;
-    constexpr auto cc = tudb::is_unique_values<1, 1.f, samples::NAME, true, false>::value;
-    constexpr auto dd = tudb::is_unique_values<1, 1.f, samples::NAME, true, false, 1.f>::value;
-    constexpr auto ee = tudb::is_unique_values<1, 1.f, samples::NAME, true, false, 0, 2.f>::value;
     constexpr auto ff = tudb::cstr{"abc"} == tudb::cstr{"abcd"};
-    std::cout << (aa ? "true" : "fals") << (ff ? "true" : "fals");
+    std::cout << (ff ? "true" : "fals");
 
     using types1 = tudb::type_list<int, double, std::string>;
     using types2 = tudb::type_list<std::nullptr_t, std::size_t, char>;
