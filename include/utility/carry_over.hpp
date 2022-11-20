276bd86fc47b9952bacb82b083bcb8964bad20a5
mizuno <mifukata-nozumi@outlook.jp>
2022-11-21 07:59:20 +0900

    変数名変更、不要変数削除

--- include/utility/carry_over.hpp
+++ include/utility/carry_over.hpp
@@ -20,12 +20,12 @@ namespace tudb
          * @brief 同じ型が渡されたとき
          * @param f 目的の処理
          * @param arg 引数
-         * @param assign_offset 代入先のインデックスにずれがあるとき指定
+         * @param offset 代入先のインデックスにずれがあるとき指定
         */
         template <std::size_t N>
-        constexpr carry_over_container run(CarryOverCallable<T> auto f, const carry_over_container<T, N>& arg, const unsigned int assign_offset = 0) const
+        constexpr carry_over_container run(CarryOverCallable<T> auto f, const carry_over_container<T, N>& arg, const unsigned int offset = 0) const
         {
-            return run(f, arg, f, assign_offset);
+            return run(f, arg, f, offset);
         }
 
         /**
@@ -34,18 +34,18 @@ namespace tudb
          * @param f 目的の処理
          * @param arg 引数
          * @param carry_over_process 桁上がりに対して実施される処理
-         * @param assign_offset 代入先のインデックスにずれがあるとき指定
+         * @param offset 代入先のインデックスにずれがあるとき指定
         */
         template <std::size_t N>
         constexpr carry_over_container run(
             CarryOverCallable<T> auto f,
             const carry_over_container<T, N>& arg,
             CarryOverCallable<T> auto carry_over_process,
-            const unsigned int assign_offset = 0
+            const unsigned int offset = 0
         ) const {
             auto result = (*this);
             for (unsigned int i = 0; i < (std::min)(Size, N); i++)
-                result = result.run(f, arg[i], carry_over_process, i, assign_offset);
+                result = result.run(f, arg[i], carry_over_process, i + offset);
             return result;
         }
 
@@ -55,11 +55,11 @@ namespace tudb
          * @param f 目的の処理
          * @param arg 引数
          * @param offset 計算対象のインデックスを指定
-         * @param assign_offset 代入先のインデックスにずれがあるとき指定
+         * @param offset 代入先のインデックスにずれがあるとき指定
         */
-        constexpr carry_over_container run(CarryOverCallable<T> auto f, const T& arg, unsigned int offset = 0, const unsigned int assign_offset = 0) const
+        constexpr carry_over_container run(CarryOverCallable<T> auto f, const T& arg, unsigned int offset = 0) const
         {
-            return run(f, arg, f, offset, assign_offset);
+            return run(f, arg, f, offset);
         }
 
         /**
@@ -69,20 +69,16 @@ namespace tudb
          * @param arg 引数
          * @param carry_over_process 桁上がりに対して実施される処理
          * @param offset 計算対象のインデックスを指定
-         * @param assign_offset 代入先のインデックスにずれがあるとき指定
+         * @param offset 代入先のインデックスにずれがあるとき指定
         */
         constexpr carry_over_container run(
             CarryOverCallable<T> auto f,
             const T& arg,
             CarryOverCallable<T> auto carry_over_process,
-            const unsigned int offset = 0,
-            const unsigned int assign_offset = 0
+            const unsigned int offset = 0
         ) const {
             auto result = (*this);
             if (offset < Size) {
-                if (assign_offset > 0)
-                    return run(carry_over_process, arg, offset + assign_offset);
-
                 auto calclated = f(this->data()[offset], arg);
                 result[offset] = calclated.first[0];
 
@@ -127,9 +123,9 @@ namespace tudb
          * @fn
          * @brief 引数をすべての要素に適用する
         */
-        constexpr carry_over_container run_all(CarryOverCallable<T> auto f, const T& arg, const unsigned int assign_offset = 0) const
+        constexpr carry_over_container run_all(CarryOverCallable<T> auto f, const T& arg, const unsigned int offset = 0) const
         {
-            return run_all(f, arg, f, assign_offset);
+            return run_all(f, arg, f, offset);
         }
 
         /**
@@ -140,7 +136,7 @@ namespace tudb
             CarryOverCallable<T> auto f,
             const T& arg,
             CarryOverCallable<T> auto carry_over_process,
-            const unsigned int assign_offset = 0
+            const unsigned int offset = 0
         ) const {
             auto result = carry_over_container{};
             for (unsigned int i = 0; i < Size; i++) {
@@ -148,7 +144,7 @@ namespace tudb
                 result = result.run(
                     carry_over_process,
                     carry_over_container<T, 2>{calclated.first[0], calclated.first[1]},
-                    i + assign_offset
+                    i + offset
                 );
             }
             return result;
