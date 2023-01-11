a4e3506fd52c79db3f0c058bdabe18ad6590a46b
mizuno <mifukata-nozumi@outlook.jp>
2023-01-12 00:24:47 +0900

    tuutilへcstr移動

---
+++ include/tuutilcpp/str.hpp
@@ -0,0 +1,13 @@
+///----------------------------------
+/// @file str.hpp 
+/// @brief コンパイル時文字列/正規表現
+///----------------------------------
+#ifndef TUUTILCPP_INCLUDE_GUARD_STR_META_HPP
+#define TUUTILCPP_INCLUDE_GUARD_STR_META_HPP
+
+#include <tuutilcpp/utility.hpp>
+#include <tuutilcpp/mpl.hpp>
+
+#include <tuutilcpp/str/cstr.hpp>
+
+#endif // TUUTILCPP_INCLUDE_GUARD_STR_META_HPP
