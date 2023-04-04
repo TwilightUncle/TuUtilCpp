ac700bb9d28679a1924ad22aecd810108d420afc
mizuno <mifukata-nozumi@outlook.jp>
2023-04-04 22:08:51 +0900

    is_integralあたりの制約に引っ掛かっている様なので順番を変えてみる

--- include/tuutilcpp/utility/algorithm.hpp
+++ include/tuutilcpp/utility/algorithm.hpp
@@ -12,7 +12,7 @@ namespace tuutil::utility
      * @brief テンプレート引数で渡した整数値の桁数を取得する。第二引数は任意の進数を指定する
     */
     template <auto V, std::size_t Hex = 10>
-    requires (Hex >= 2 && (std::is_integral_v<decltype(V)> || is_big_int_v<decltype(V)>))
+    requires (Hex >= 2 && (is_big_int_v<decltype(V)> || std::is_integral_v<decltype(V)>))
     constexpr auto get_digit()
     {
         std::size_t digit = 1;
