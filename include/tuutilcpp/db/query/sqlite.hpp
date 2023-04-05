196746c988ae548094db006c192bf1590c5d3e8d
mizuno <mifukata-nozumi@outlook.jp>
2023-04-05 18:49:17 +0900

    確認用

--- include/tuutilcpp/db/query/sqlite.hpp
+++ include/tuutilcpp/db/query/sqlite.hpp
@@ -66,11 +66,13 @@ namespace tuutil::db::query
         */
         template <ColumnDefinable ColumnDefinition>
         using make_column_define_string = mpl::value_constant<
-            ('"' + ColumnDefinition::name + "\" "
-                + make_type_name_string<typename ColumnDefinition::field_type>::value + ' '
-                + make_auto_increment_string<ColumnDefinition::auto_increment>()
-                + make_not_null_string<ColumnDefinition::not_null>()
-            ).template remove_suffix<1>()
+            []() {
+                return ('"' + ColumnDefinition::name + "\" "
+                    + make_type_name_string<typename ColumnDefinition::field_type>::value + ' '
+                    + make_auto_increment_string<ColumnDefinition::auto_increment>()
+                    + make_not_null_string<ColumnDefinition::not_null>()
+                ).template remove_suffix<1>();
+            }()
         >;
         template <ColumnDefinable ColumnDefinition>
         static constexpr auto make_column_define_string_v = make_column_define_string<ColumnDefinition>::value;
