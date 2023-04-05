84bf30cbd3ab99bff00491d43f33b54145713c2e
mizuno <mifukata-nozumi@outlook.jp>
2023-04-05 18:28:33 +0900

    場所移動したらgccのコンパイル通るか確認

--- tests/tuutilcpp/db_test.cpp
+++ tests/tuutilcpp/db_test.cpp
@@ -340,8 +340,6 @@ TEST(TuutilcppDbTest, SqliteQueryTest)
     constexpr auto case5 = sqlite_query::make_column_define_string<column_id>::value.data();
     constexpr auto case6 = sqlite_query::make_column_define_string<column_na>::value.data();
 
-    // EXPECT_STREQ(case5, R"("id" int autoincrement not null)");
-    // EXPECT_STREQ(case6, R"("name" varchar(255))");
 
     using table1 = db::define_table<
         samples,
@@ -350,6 +348,9 @@ TEST(TuutilcppDbTest, SqliteQueryTest)
     >;
     constexpr auto case7 = sqlite_query::make_create_table_string<table1>::value.data();
     constexpr auto case8 = sqlite_query::make_drop_table_string<table1>::value.data();
+
+    EXPECT_STREQ(case5, R"("id" int autoincrement not null)");
+    EXPECT_STREQ(case6, R"("name" varchar(255))");
     EXPECT_STREQ(case7, R"(create table "samples"("id" int autoincrement not null, "name" varchar(255)))");
     EXPECT_STREQ(case8, R"(drop table "samples")");
 }
